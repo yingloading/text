9662da8e8d42546f88d4f03eb18bf91c3ce455e7
yingloading <renxine1@sina.com>
2018-03-09 14:38:50 +0800

    message "Python"

---
+++ Python/textone.py
@@ -0,0 +1,16 @@
+
+import os
+#打开印象笔记
+os.startfile("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Evernote\Evernote")
+#打开谷歌浏览器
+os.startfile("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome")
+#打开navicat
+os.startfile(r"D:\Program Files\PremiumSoft\Navicat Premium\navicat.exe")
+#打开notepad++
+os.startfile(r"D:\Program Files (x86)\Notepad++\notepad++.exe")
+#打开atom编辑器
+os.startfile(r"C:\Users\Administrator\AppData\Local\atom\app-1.24.0-beta1\atom.exe")
+#打开微信
+os.startfile(r"C:\Program Files (x86)\Tencent\WeChat\WeChat.exe")
+#打开有道翻译
+os.startfile(r"C:\Users\Administrator\AppData\Local\Youdao\dict\Application\YoudaoDict.exe")
